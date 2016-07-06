bb2e915f7eb9bdd20bd255ce9c75939f8947a7f6
sukhbir-singh <sukhbir947@gmail.com>
2016-07-06 15:36:18 +0530

    initial commit with splash

---
+++ app/src/main/java/akssmk/com/agriculturalapp/SplashActivity.java
@@ -0,0 +1,29 @@
+package akssmk.com.agriculturalapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sukhbir on 3/7/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private int SPLASH_LENGTH=1200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_LENGTH);
+    }
+}
