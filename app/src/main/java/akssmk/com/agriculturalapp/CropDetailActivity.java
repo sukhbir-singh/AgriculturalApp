43192070ee2ed004b6e86057bbfbd84e07944e43
Akhilesh Kumar <akhileshnithcse@gmail.com>
2016-08-11 16:01:09 +0530

    paddy detail is added

--- app/src/main/java/akssmk/com/agriculturalapp/CropDetailActivity.java
+++ app/src/main/java/akssmk/com/agriculturalapp/CropDetailActivity.java
@@ -19,8 +19,12 @@ public class CropDetailActivity extends AppCompatActivity {
     Integer[] headings={R.string.wheat_h1,R.string.wheat_h2,R.string.wheat_h3,R.string.wheat_h4,R.string.wheat_h5,
             R.string.wheat_h6,R.string.wheat_h7,R.string.wheat_h8,R.string.wheat_h9,R.string.wheat_h10,
             R.string.wheat_h11,R.string.wheat_h12,R.string.wheat_h13,R.string.wheat_h14};
+
     Integer[] data={R.string.wh1,R.string.wh2,R.string.wh3,R.string.wh4,R.string.wh5,R.string.wh6,R.string.wh7,
             R.string.wh8,R.string.wh9,R.string.wh10,R.string.wh11,R.string.wh12,R.string.wh13,R.string.wh14};
+
+    Integer[] data1={R.string.ph1,R.string.ph2,R.string.ph3,R.string.ph4,R.string.ph5,R.string.ph6,R.string.ph7,
+            R.string.ph8,R.string.ph9,R.string.ph10,R.string.ph11,R.string.ph12,R.string.ph13,R.string.ph14};
 
 
     @Override
