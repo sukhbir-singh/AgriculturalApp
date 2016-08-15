8deb041f483aab7a8430792a0380ce467688686a
akhiles26 <akhileshnithcse@gmail.com>
2016-08-15 22:18:43 +0530

    paddy productin working

--- app/src/main/java/akssmk/com/agriculturalapp/activities/CropDetailActivity.java
+++ app/src/main/java/akssmk/com/agriculturalapp/activities/CropDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -25,10 +26,10 @@ public class CropDetailActivity extends AppCompatActivity {
             R.string.wheat_h6,R.string.wheat_h7,R.string.wheat_h8,R.string.wheat_h9,R.string.wheat_h10,
             R.string.wheat_h11,R.string.wheat_h12,R.string.wheat_h13,R.string.wheat_h14};
 
-    Integer[] data={R.string.wh1,R.string.wh2,R.string.wh3,R.string.wh4,R.string.wh5,R.string.wh6,R.string.wh7,
+    Integer[] data1={R.string.wh1,R.string.wh2,R.string.wh3,R.string.wh4,R.string.wh5,R.string.wh6,R.string.wh7,
             R.string.wh8,R.string.wh9,R.string.wh10,R.string.wh11,R.string.wh12,R.string.wh13,R.string.wh14};
 
-    Integer[] data1={R.string.ph1,R.string.ph2,R.string.ph3,R.string.ph4,R.string.ph5,R.string.ph6,R.string.ph7,
+    Integer[] data2={R.string.ph1,R.string.ph2,R.string.ph3,R.string.ph4,R.string.ph5,R.string.ph6,R.string.ph7,
             R.string.ph8,R.string.ph9,R.string.ph10,R.string.ph11,R.string.ph12,R.string.ph13,R.string.ph14};
 
 
@@ -36,6 +37,15 @@ public class CropDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_recycler);
+
+        Integer[] temp=null;
+
+        int num=(int)getIntent().getIntExtra("number",1);
+        if(num==1){
+            temp=data1;
+        }else if(num==2){
+            temp=data2;
+        }
 
         mRecyclerView=(RecyclerView)findViewById(R.id.recycler);
         bar=(ProgressBar)findViewById(R.id.progress);
@@ -45,7 +55,7 @@ public class CropDetailActivity extends AppCompatActivity {
         {
             CropDetailItem item=new CropDetailItem();
             item.setHeading(headings[i]);
-            item.setDetail(data[i]);
+            item.setDetail(temp[i]);
             items.add(item);
         }
 
